09dc707d8aa79ef2ddd2700c451317b03fbe0aee
Bethina Dileep <56349269+Bethinadileep@users.noreply.github.com>
2022-08-29 20:28:08 +0530

    Create Swtich_Program.cs

---
+++ Swtich_Program.cs
@@ -0,0 +1,31 @@
+
+using System;
+class HelloWorld {
+  static void Main(string[] args) {
+    int day = 2;
+    switch(day)
+    {
+        case 1:
+            Console.WriteLine("Monday");
+            break;
+        case 2:
+            Console.WriteLine("Tuseday");
+            break;
+        case 3:
+            Console.WriteLine("Wednesday");
+            break;
+        case 4:
+            Console.WriteLine("Thursday");
+            break;
+        case 5:
+            Console.WriteLine("Friday");
+            break;
+        case 6:
+            Console.WriteLine("Saturday");
+            break;
+        case 7:
+            Console.WriteLine("Sunday");
+            break;
+    }
+  }
+}
