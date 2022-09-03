0423c5a4b2332a346476945a2961d3760d4c86c2
Bethina Dileep <56349269+Bethinadileep@users.noreply.github.com>
2022-09-03 11:55:54 +0530

    Create Finding_Element_in_Array.java

---
+++ Finding_Element_in_Array.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		int arr[] = {1,2,3,4,5};
+		int k = 3;
+		for(int i = 0; i < arr.length; i++)
+		{
+		    if(arr[i] == k)
+		    {
+		        System.out.println(i);
+		        break;
+		    }
+		}
+	}
+}
