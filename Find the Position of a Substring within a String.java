4febe7d5d0cf3bbd672b6c4718fc77e37d26bd3b
Bethina Dileep <56349269+Bethinadileep@users.noreply.github.com>
2022-09-09 10:14:54 +0530

    Create Find the Position of a Substring within a String.java

---
+++ Find the Position of a Substring within a String.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main
+{
+    public static int foundpattern(String text,String pattern){
+        int n = text.length();
+        int m = pattern.length();
+        
+        for(int i = 0; i < n; i++){
+            int temp = i;
+            int j = 0;
+            for(j = 0; j < m; j++){
+                if(text.charAt(temp) != pattern.charAt(j)){
+                    break;
+                }
+                temp++;
+            }
+            if(j == m){
+                return i;
+            }
+        }
+        return -1;
+    }
+	public static void main(String[] args) {
+		String text = "takeuforward";
+		String pattern = "forward";
+		
+		int foundIdx = foundpattern(text, pattern);
+		
+		System.out.println(foundIdx);
+	}
+}
